d91c1b46b27ba027fa6ed92ab0f5c2438b494fcc
Semyon <2001saimon1002@gmail.com>
2025-03-03 11:57:13 +0400

    refactor: update path import in next.config.ts for alias configuration

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,22 @@
+import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    eslint: {
-        dirs: ['src'],
-    },
-    compiler: {
-        emotion: true,
-    },
-    // Настройка alias для упрощения импортов
-    webpack(config: import('webpack').Configuration) {
-        config.resolve = config.resolve || {};
-        config.resolve.alias = {
-            ...config.resolve.alias || {},
-            '@': require('path').resolve(__dirname, 'src'),
-        };
-        return config;
-    },
+  reactStrictMode: true,
+  eslint: {
+    dirs: ['src'],
+  },
+  compiler: {
+    emotion: true,
+  },
+  // Настройка alias для упрощения импортов
+  webpack(config: import('webpack').Configuration) {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
